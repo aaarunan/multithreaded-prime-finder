d4d06aa906d9444a26f7d6b02217a051dcd4f1a0
aaarunan <arunan2212@gmail.com>
2023-01-11 11:04:42 +0100

    Decouple is_prime method

--- main.cpp
+++ main.cpp
@@ -8,33 +8,26 @@ using namespace std;
 
 mutex mtx;
 
-vector<int> findPrimeNumbersInterval(int start, int end) {
-    //check if number is even
-    if (start ^ (1 == start + 1)) {
-        start = start + 1;
+bool isPrime(int num) {
+    if (num <= 2) {
+        return false;
+    }
+    if ((num & 1) == 0) {
+       return false;
     }
 
-    vector<int> primes;
+    int num_sqrt = (int) sqrt(num);
 
-    for (int target = start; target < end; target += 2) {
-        bool isPrime = true;
-        int target_squared = (int) sqrt(target);
-
-        for (int i = 3; i < target_squared; i += 2) {
-            if (target % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime) {
-            primes.emplace_back(target);
+    for (int i = 3; i <= num_sqrt; i += 2) {
+        if (num % i == 0) {
+            return false;
         }
     }
-    return primes;
+
+    return true;
 }
 
-void printResults(double duration, const vector<int>& primes) {
+void printResults(double duration, const vector<int> &primes) {
     cout << "Program finish.\n";
     cout << "Time taken:      " << duration << " s\n";
     cout << "Found :          " << primes.size() << " primes \n";
@@ -43,25 +36,37 @@ void printResults(double duration, const vector<int>& primes) {
 
 int main() {
     int start = 0;
-    int end = 50000000;
+    int end = 10000000;
     int intervalLength = 20000;
 
 
     vector<thread> threads;
     vector<int> primes;
-
+    for (int i = 1; i < 100; i++) {
+        if (isPrime(i)) {
+            cout << i << "\n";
+        }
+    }
 
     auto start_time = chrono::steady_clock::now();
     int i = 0;
+
 
     cout << "Finding primes between " << start << " and " << end << "...\n";
 
     while (i < end) {
         threads.emplace_back(
                 [i, &intervalLength, &primes] {
-                    vector<int> found_primes = findPrimeNumbersInterval(i, i + intervalLength);
-                    std::lock_guard<std::mutex> lock(mtx);
-                    primes.insert(primes.end(), found_primes.begin(), found_primes.end());
+                    int start = i;
+                    if (i ^ (1 == i + 1)) {
+                        start++;
+                    }
+                    for (int j = start; j < start + intervalLength; j += 2) {
+                        if (isPrime(j)) {
+                            std::lock_guard<std::mutex> lock(mtx);
+                            primes.push_back(j);
+                        }
+                    }
                 });
 
         i += intervalLength;
