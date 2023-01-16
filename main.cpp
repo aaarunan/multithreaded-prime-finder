1609044e44d4fc107608ed65351755e7b87d6c4c
aaarunan <arunan2212@gmail.com>
2023-01-16 02:58:09 +0100

    Make lamda call function instead

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ using namespace std;
 mutex mtx;
 
 bool isPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    if (num == 2) {
-        return true;
-    }
-    if ((num & 1) == 0) {
-        return false;
-    }
+    //Not needed in a controlled environment
+    //if (num == 2) {
+    //    return true;
+    //}
+    //if ((num & 1) == 0) {
+    //    return false;
+    //}
+    //if (num <= 1) {
+    // return false;
+    //}
 
     int num_sqrt = (int) sqrt(num);
 
@@ -30,6 +31,23 @@ bool isPrime(int num) {
     return true;
 }
 
+void findPrimes(int num, int intervalLength, vector<int> &primes) {
+    int start = num;
+
+    //check if number is even except number 2
+    if ((num & 1) == 0) {
+        start++;
+    }
+
+
+    for (int j = start; j < start + intervalLength; j += 2) {
+        if (isPrime(j)) {
+            std::lock_guard<std::mutex> lock(mtx);
+            primes.emplace_back(j);
+        }
+    }
+}
+
 void printResults(double duration, const vector<int> &primes) {
     cout << "Program finish.\n";
     cout << "Time taken:      " << duration << " s\n";
@@ -37,45 +55,46 @@ void printResults(double duration, const vector<int> &primes) {
 
 }
 
-int main() {
-    int start = 0;
-    int end = 10000000;
-    int intervalLength = 20000;
-
-
-    vector<thread> threads;
-    vector<int> primes;
-    for (int i = 1; i < 100; i++) {
+void test() {
+    for (int i = 1; i < 10; i++) {
         if (isPrime(i)) {
             cout << i << "\n";
         }
     }
+}
 
-    auto start_time = chrono::steady_clock::now();
+int main() {
+    int start = 2;
+    int end = 100000000;
+    int intervalLength = 100000;
+
+    //test();
+
     int i = 0;
-
+    vector<thread> threads;
+    vector<int> primes;
 
     cout << "Finding primes between " << start << " and " << end << "...\n";
 
-    while (i < end) {
-        threads.emplace_back(
-                [i, &intervalLength, &primes] {
-                    int start = i;
-                    if (i ^ (1 == i + 1)) {
-                        start++;
-                    }
-                    for (int j = start; j < start + intervalLength; j += 2) {
-                        if (isPrime(j)) {
-                            std::lock_guard<std::mutex> lock(mtx);
-                            primes.push_back(j);
-                        }
-                    }
-                });
+    auto start_time = chrono::steady_clock::now();
 
+    if (start <= 2) {
+        primes.push_back(2);
+    }
+
+    //Create threads with an array of numbers with length intervalLength
+    while (i < end) {
+        threads.emplace_back([i, intervalLength, &primes] { findPrimes(i, intervalLength, primes); });
         i += intervalLength;
     }
 
-    for (auto &thread: threads) {
+    //Do the rest of the numbers if there are any
+    if (i != end) {
+        int rest = end - i;
+        threads.emplace_back([i, rest, &primes] { findPrimes(i, rest, primes); });
+    }
+
+    for (auto &thread : threads) {
         thread.join();
     }
 
@@ -83,6 +102,7 @@ int main() {
     std::chrono::duration<double> duration = end_time - start_time;
 
     printResults(duration.count(), primes);
+
     return 0;
 }
 
